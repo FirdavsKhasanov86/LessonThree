cb97c319c2c21d06f81202db08367875acf618bb
user <user@Williams-iMac.local>
2020-03-25 22:38:42 +0500

    HomeWork3

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           //HomeWork3
+
+            //2.
+            //  double x=8,y=4;
+            // if(x!=y)
+            // {
+            //    if(x - y > 0){
+                   
+            //        Console.WriteLine("x = " + x);
+            //        Console.WriteLine("y = " + x);
+            //    }
+            //    if(x - y < 0){
+                  
+            //        Console.WriteLine("x = " + y);
+            //        Console.WriteLine("y = " + y);
+            //    }
+            // }
+            // if(x==y)
+            // {
+            //     x = 0;
+            //     y = 0;
+            //     Console.WriteLine("x = " + x);
+            //     Console.WriteLine("y = " + y);
+            // }
+               
+           //-----------------------------------------//
+
+            //3. 
+         
+            // double operand1,operand2;
+            // string sing;
+            // Console.WriteLine("Введите число 1");
+            // operand1 = double.Parse(Console.ReadLine());
+
+            // Console.WriteLine("Введите число 2");
+            // operand2 = double.Parse(Console.ReadLine());
+
+            // Console.WriteLine("Вибирите операцию: '*' '/' '+' '/'" );
+            // sing =Console.ReadLine();
+
+            // switch (sing)
+            // {
+            //     case  "+":
+            //          Console.WriteLine(operand1 + operand2);
+            //          break;
+            //     case  "-":
+            //          Console.WriteLine(operand1 - operand2);
+            //          break;
+            //     case  "*":
+            //          Console.WriteLine(operand1 * operand2);
+            //          break;
+            //     case  "/":
+            //          if (operand1 == 0 || operand2 == 0) 
+            //         {
+            //          Console.WriteLine("Ошибка, данное число не делится на 0");
+            //          break;
+            //         }
+            //         else 
+            //         {
+            //           Console.WriteLine(operand1 / operand2);   
+            //         }break;
+            //     default:
+            //              Console.WriteLine("Ошибка");
+            //              break; 
+            // }
+ 
+             //---------------------------------------------------------//
+
+            //4.
+            
+            //  Console.WriteLine("Выберите 1 то 100");
+            //  double num = double.Parse(Console.ReadLine());
+            //  if(num <=100){
+
+            //      if(num>=0 && num<=14)
+            //      {
+            //        Console.WriteLine("[0-14]");
+            //      }
+            //      if(num>=15 && num <=35)
+            //      {
+            //        Console.WriteLine("[15-35]");
+            //      }
+            //      if(num>=36 && num<=50)
+            //      {
+            //       Console.WriteLine("[36-50]");
+            //      }
+            //      if(num>=51 && num<=100)
+            //      {
+            //        Console.WriteLine("[51-100]");
+            //      }
+            // }
+            // else{
+            //   Console.WriteLine("Введенное число не вход в диапахон от 1 до 100");
+            // }
+             
+        }
+    }
+}
